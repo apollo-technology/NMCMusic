bb63ea4d3179eabd07d36b2e9a86efb33d3a9679
apollo-technology <admin@apollotechnology.co>
2017-02-01 09:23:33 -0500

    Icons

--- NMCMusic/NMCMusic/InitialVC.m
+++ NMCMusic/NMCMusic/InitialVC.m
@@ -20,7 +20,16 @@
     // Do any additional setup after loading the view.
     [ATRuntime getAll:^(BOOL succeeded, NSError *error) {
         if (succeeded) {
-            [self goHome];
+            NSArray *allowedVersions = [[[ATRuntime data] config] objectForKey:@"allowedVersions"];
+            NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
+            NSString *version = [infoDictionary objectForKey:@"CFBundleShortVersionString"];
+            if ([allowedVersions containsObject:version]) {
+                [self goHome];
+            } else {
+                UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Update" message:@"There is an update available. Please go to the app store and update this app." preferredStyle:UIAlertControllerStyleAlert];
+                [self presentViewController:alertController animated:YES completion:nil];
+
+            }
         } else {
             UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Error" message:error.localizedDescription preferredStyle:UIAlertControllerStyleAlert];
             [alertController addAction:[UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:nil]];
